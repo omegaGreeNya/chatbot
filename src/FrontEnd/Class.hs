dca1a44b4e432c19f515d736f160c22506ec9f4d
omegaGreeNya <megamanCake@gmail.com>
2022-07-21 20:07:28 +0500

    Splited Front class and FrontMessage, for more flexible instace defining

--- src/FrontEnd/Class.hs
+++ src/FrontEnd/Class.hs
@@ -13,22 +13,24 @@ import Message (Message(..))
 import User.Class (BotUser(..))
 import qualified ChatBot (Handle, Response, Event)
 
--- | Glue class to connect user representation, frontend methods and bot logic.
--- This is inner class, you don't need to provide logging functional for methods of this class.
--- frontHandle should contain all info to construct BotHandle, with provided UserId.
--- Also, you can use frontHandle to store host adress, botkey, etc.
--- All data you need to work with API may be stored inside this handle.
+-- | Related to Front class data family.
+-- Represents Message format of particular Frontend.
+-- For example of instance see API.Telegram.Parse
+data family FrontMessage :: Type
+
+-- | Glue class to connect user representation, frontend methods calls,
+-- Message data and bot logic.
+-- All data you need to work with API should be stored inside frontHandle.
+-- Consider this class as inner. So you don't need to provide logging functional
+-- for methods of this class, unless you want to debug.
 class ( BotUser user m idType
       , Monad m
-      , Message (FrontMessage frontHandle))
+      , Message FrontMessage)
    => Front frontHandle m user idType | user -> idType where
-   data FrontMessage frontHandle :: Type
-   -- ^ Associated Message type, to work not only with message text, 
-   -- but also with metadata about message (markup for example).
-   getMessages     :: frontHandle -> m [(UserId user, ChatBot.Event (FrontMessage frontHandle))]
+   getMessages     :: frontHandle -> m [(UserId user, ChatBot.Event FrontMessage)]
    -- ^ Gets list of new events from API.
    -- If you track offset, change it inside this funtion.
-   sendResponse    :: frontHandle -> (UserId user, ChatBot.Response (FrontMessage frontHandle)) -> m ()
+   sendResponse    :: frontHandle -> (UserId user, ChatBot.Response FrontMessage) -> m ()
    -- ^ Sends bot answer to the user
    createBotHandle :: frontHandle -> UserId user -> m (Maybe (ChatBot.Handle m))
    -- ^ Creates bot handle with provided @UserId@
