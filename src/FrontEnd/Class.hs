154182594a5ffff1321943febfa2ee5f2e1302cd
omegaGreeNya <megamanCake@gmail.com>
2022-07-23 17:11:59 +0500

    Simplified Front class

--- src/FrontEnd/Class.hs
+++ src/FrontEnd/Class.hs
@@ -14,23 +14,16 @@ module FrontEnd.Class
    (Front(..)) where
 
 import Message (Message(..))
-import User.Class (BotUser(..))
-import qualified ChatBot (Handle, Response, Event)
+import qualified ChatBot (Response, Event)
 
--- | Glue class to connect user representation, frontend methods calls,
--- Message data and bot logic.
+-- | Class defines methods to get users input and send answers back.
 -- All data you need to work with API should be stored inside frontHandle.
--- Consider this class as inner. So you don't need to provide logging functional
--- for methods of this class, unless you want to debug.
-class ( BotUser user userIdType m
-      , Monad m
-      , Message frontMessage)
-   => Front frontHandle frontMessage user userIdType m
-      | frontHandle -> frontMessage, user -> userIdType where
-   getMessages     :: frontHandle -> m [(UserId user, ChatBot.Event frontMessage)]
-   -- ^ Gets list of new events from API.
+class ( Message frontMessage
+      , Monad m)
+   => Front frontHandle frontMessage userIdType m
+      | frontHandle -> frontMessage, frontHandle -> userIdType where
+   getMessages     :: frontHandle -> m [(userIdType, ChatBot.Event frontMessage)]
+   -- ^ Gets list of new events.
    -- If you track offset, change it inside this funtion.
-   sendResponse    :: frontHandle -> (UserId user, ChatBot.Response frontMessage) -> m ()
+   sendResponse    :: frontHandle -> (userIdType, ChatBot.Response frontMessage) -> m ()
    -- ^ Sends bot answer to the user
-   createBotHandle :: frontHandle -> UserId user -> m (Maybe (ChatBot.Handle m))
-   -- ^ Creates bot handle with provided @UserId@
